6902adced77f71536abd992535acb0963ef2e847
bias <trevor.bain@gmail.com>
2011-04-19 14:29:58 -0500

    finished ch2 - not amazing, but working with autotools

---
+++ ch02/ex13/log.c
@@ -0,0 +1,91 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "log.h"
+
+#define TRAV_INIT_SIZE 8
+
+typedef struct list_struct {
+  data_t item;
+  struct list_struct *next;
+} log_t;
+
+static log_t *headptr = NULL;
+static log_t *tailptr = NULL;
+
+int addmsg(data_t data) {
+  log_t *newnode;
+  int nodesize;
+
+  nodesize = sizeof(log_t) + strlen(data.string) + 1;
+  if ((newnode = (log_t *)(malloc(nodesize))) == NULL) 
+    return -1;
+  newnode->item.time = data.time;
+  newnode->item.string = (char *)newnode + sizeof(log_t);
+  strcpy(newnode->item.string, data.string);
+  newnode->next = NULL;
+  if (headptr == NULL)
+    headptr = newnode;
+  else 
+    tailptr->next = newnode;
+  tailptr = newnode;
+  return 0;
+}
+
+void clearlog(void) {
+  log_t *travptr;
+  while ( headptr->next != NULL ) {
+    travptr = headptr;
+    headptr = headptr->next;
+    free(travptr);
+  }
+  free(headptr);
+}
+
+char *getlog(void) { 
+  /* allocate space for string with entire log, return pointer to string */
+  log_t *travptr;
+  int strsize;
+  char *wholelog;
+
+  strsize = 0;
+  travptr = headptr;
+  while ( travptr != NULL ) {
+    /* count char plus 2 bytes for newline */
+    strsize += strlen( travptr->item.string ) + 2;
+    travptr = travptr->next;
+  }
+
+  wholelog = malloc(strsize + 1);
+  travptr = headptr;
+  while ( travptr != NULL ) {
+    if ( travptr == headptr )
+      strcat(strcpy(wholelog, travptr->item.string), "\n");
+    else
+      strcat(strcat(wholelog, travptr->item.string), "\n");
+    travptr = travptr->next;
+  }
+
+  return wholelog;
+}
+
+int savelog(char *filename) {
+  FILE *fp;
+  char *log;
+
+  fp = fopen(filename, "w");
+
+  if ( fp == NULL ) {
+    fprintf(stderr, "Can't open file!\n");
+    return -1;
+  } 
+  else {
+    log = getlog();
+    fprintf(fp, "%s", log);
+    free(log);
+  }
+
+  return 0;
+}
--- ch02/ex13/test.c
+++ ch02/ex13/test.c
@@ -1,11 +1,30 @@
 #include "log.h"
 
-data_t log_;
+#include "stdlib.h" /* malloc */
 
 int main() { 
  
-  if (time(&(log_.time)) == -1) 
+  data_t logs;
+  char msg[5] = "test"; 
+  char *log;
+  char logname1[11] = "test_3.log";
+  char logname2[11] = "test_0.log";
+
+  if (time(&(logs.time)) == -1) 
     return -1;
-  addmsg(log_);
+  logs.string = msg;
+
+  addmsg(logs);
+  addmsg(logs);
+  addmsg(logs);
   
+  log = getlog();
+  savelog(logname1); 
+
+  clearlog();
+  
+  log = getlog();
+  savelog(logname2); 
+
+  return 0;
 }
