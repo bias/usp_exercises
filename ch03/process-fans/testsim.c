6d373f85ef0f58a7fc007cde0f6bcc898c1895b1
bias <trevor.bain@gmail.com>
2011-05-06 18:07:47 -0500

    cleaned up testsim.c

--- ch03/process-fans/testsim.c
+++ ch03/process-fans/testsim.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
+#include <stdio.h> /* fprintf */
+#include <stdlib.h> /* atoi */
+#include <unistd.h> /* sleep */
 
 int main(int argc, char *argv[]) {
 	int n, m;	
+
+	if ( argc != 3 ) {
+		fprintf(stderr, "Usage %s sleep_time num_loops\n", argv[0]);
+		return 1;
+	}
+
 	n = atoi(argv[1]);
+
 	for ( m = atoi(argv[2]); m > 0; m--) {
 		sleep(n);
 		fprintf(stderr, "pid: %ld\n", (long)getpid());
 	}
+
 	return 0;
 }
