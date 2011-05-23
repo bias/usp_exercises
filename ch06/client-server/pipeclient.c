55146365be13ba775f7eee8e1f11d0cd995bfc16
bias <trevor.bain@gmail.com>
2011-05-23 18:10:09 -0500

    Finished ch06 client-server, left in stderr cruff printing

--- ch06/client-server/pipeclient.c
+++ ch06/client-server/pipeclient.c
@@ -4,37 +4,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+/*#include <time.h>*/
 #include <unistd.h>
 #include <sys/stat.h>
 
 #include <restart.h>
 
 #define FIFOARG 1
+#define FIFO_PERMS (S_IRWXU | S_IWGRP| S_IWOTH)
 
 int main (int argc, char *argv[]) {
-   time_t curtime;
-   int len;
-   char requestbuf[PIPE_BUF];  
-   int requestfd;             
+	/*time_t curtime;*/
+	pid_t pid;
+	int len;
+	char buf[PIPE_BUF];  
+	char pid_name[BUFSIZ];  
+	int requestfd, replyfd;             
 
-   if (argc != 2) {  /* name of server fifo is passed on the command line */
-      fprintf(stderr, "Usage: %s fifoname\n", argv[0]);
-      return 1; 
-   }
+	/* setup pid string */
+	pid = getpid();
+	if ( snprintf(pid_name, BUFSIZ, "%i", (int)pid) == -1 ) {
+		perror("Client failed to write pid to buffer");
+		return 1;
+	}
 
-   if ((requestfd = open(argv[FIFOARG], O_WRONLY)) == -1) {
-       perror("Client failed to open log fifo for writing");
-       return 1; 
-   }
- 
-   curtime = time(NULL);
-   snprintf(requestbuf, PIPE_BUF, "%d: %s", (int)getpid(), ctime(&curtime));
-   len = strlen(requestbuf);
-   if (r_write(requestfd, requestbuf, len) != len) {
-      perror("Client failed to write");
-      return 1;
-   }
-   r_close(requestfd);
-   return 0; 
+	/* name of client fifo is passed on the command line */
+	if (argc != 2) {  
+		fprintf(stderr, "Usage: %s fifoname\n", argv[0]);
+		return 1; 
+	}
+
+	/* open request pipe */
+	if ((requestfd = open(argv[FIFOARG], O_WRONLY)) == -1) {
+		perror("Client failed to open request FIFO");
+		return 1; 
+	}
+	fprintf(stderr, "Client opened request FIFO: %s\n", argv[FIFOARG]);
+
+	/* create reply pipe and open for reading */
+	if ((mkfifo(pid_name, FIFO_PERMS) == -1) && (errno != EEXIST)) {
+		perror("Client failed to create reply FIFO");
+		return 1; 
+	}
+	fprintf(stderr, "Client created reply FIFO: %s\n", pid_name);
+
+	snprintf(buf, PIPE_BUF, "%d", (int)pid);
+	len = strlen(buf);
+	if (r_write(requestfd, buf, len) != len) {
+		perror("Client failed to write to request FIFO");
+		return 1;
+	}
+	fprintf(stderr, "Client wrote request: %s\n", buf);
+
+	if ((replyfd = open(pid_name, O_RDONLY)) == -1) {
+		perror("Client failed to open reply FIFO");
+		return 1;
+	}
+	fprintf(stderr, "Client opened reply FIFO: %s\n", pid_name);
+
+	r_read(replyfd, buf, PIPE_BUF); 
+	fprintf(stderr, "Client got reply: %s", buf); /* ctime includes newline */
+
+	r_close(replyfd);
+	unlink(pid_name);
+	r_close(requestfd);
+	
+	return 0; 
 }
--- ch06/client-server/pipeserver.c
+++ ch06/client-server/pipeserver.c
@@ -2,7 +2,9 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <limits.h>
 #include <sys/stat.h>
 
 #include <restart.h>
@@ -11,22 +13,54 @@
 #define FIFO_PERMS (S_IRWXU | S_IWGRP| S_IWOTH)
 
 int main (int argc, char *argv[]) {
-   int requestfd;            
-   
-   if (argc != 2) {    /* name of server fifo is passed on the command line */
-      fprintf(stderr, "Usage: %s fifoname > logfile\n", argv[0]);
-      return 1; 
-   }
-                         /* create a named pipe to handle incoming requests */
-   if ((mkfifo(argv[FIFOARG], FIFO_PERMS) == -1) && (errno != EEXIST)) {
-       perror("Server failed to create a FIFO");
-       return 1; 
-   }
-                    /* open a read/write communication endpoint to the pipe */
-   if ((requestfd = open(argv[FIFOARG], O_RDWR)) == -1) {
-       perror("Server failed to open its FIFO");
-       return 1;
-   }
-   copyfile(requestfd, STDOUT_FILENO); 
-   return 1; 
+	time_t curtime;
+	char buf[PIPE_BUF];
+	int requestfd, replyfd;
+	int len;
+
+	/* name of server fifo is passed on the command line */
+	if (argc != 2) {    
+		fprintf(stderr, "Usage: %s fifoname > logfile\n", argv[0]);
+		return 1; 
+	}
+
+	/* create a named pipe to handle incoming requests */
+	if ((mkfifo(argv[FIFOARG], FIFO_PERMS) == -1) && (errno != EEXIST)) {
+		perror("Server failed to create request FIFO");
+		return 1; 
+	}
+	fprintf(stderr, "Server created FIFO: %s\n", argv[FIFOARG]);
+
+	/* open a read/write communication endpoint to the pipe */
+	if ((requestfd = open(argv[FIFOARG], O_RDWR)) == -1) {
+		perror("Server failed to open request FIFO");
+		return 1;
+	}
+
+	while ( 1 ) {
+		r_read(requestfd, buf, PIPE_BUF); 
+		fprintf(stderr, "Server got request pid: %s\n", buf);
+
+		if ((replyfd = open(buf, O_WRONLY)) == -1) {
+			perror("Server failed to open reply FIFO");
+			return 1;
+		}
+		fprintf(stderr, "Server opened request pipe: %s\n", buf);
+
+		curtime = time(NULL);
+		snprintf(buf, PIPE_BUF, "%d: %s", (int)getpid(), ctime(&curtime));
+		len = strlen(buf);
+		if ( r_write(replyfd, buf, len) != len ) {
+			perror("Server failed to write to request FIFO"); 
+			return 1;
+		}
+		fprintf(stderr, "Server wrote reply: %s", buf); /* ctime includes newline */
+
+		r_close(replyfd);
+	}
+
+	r_close(requestfd);
+	unlink(argv[FIFOARG]);
+
+	return 1; 
 }
